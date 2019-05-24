1d6ae37e23814b669ab7c73b5b5639d02c0e7977
Scarping <gustavo.scarpin.2020@gmail.com>
2019-05-24 19:06:53 -0300

    atualizado

---
+++ Carpooling/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carpooling.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Tela Executiva para Ajuda";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina Executiva para Contatos";
+
+            return View();
+        }
+    }
+}
